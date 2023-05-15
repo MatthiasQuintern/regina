eec707e3b4ca2ba5888d034b72cfa2770f61269b
matthias@arch <matthiasqui@protonmail.com>
2023-05-15 22:06:08 +0200

    removed parse_log

--- regina/__init__.py
+++ regina/__init__.py
@@ -1,5 +1,4 @@
 """Gather analytics from nginx access logs and visualize them through generated images and a generated html"""
 # __package__ = 'regina'
 
-from regina.data_collection import parse_log
 from regina import database
